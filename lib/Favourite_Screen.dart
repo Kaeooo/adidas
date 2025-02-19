db2f0c722a9e7741a9e8d098f6f64738ab7a004f
Kaeooo <suroivia.210846@gmail.com>
2025-02-19 16:15:18 +0700

    update 19/02/68

---
+++ lib/Favourite_Screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FavouriteScreen extends StatelessWidget {
+  const FavouriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
